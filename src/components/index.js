a1271db76314e0d3d45297e823933ae22d15c20d
User <user@Users-MacBook-Pro.local>
2021-01-04 17:49:42 -0500

    Updates

--- src/components/index.js
+++
@@ -1,4 +0,0 @@
-export { default as Chart } from './Chart/Chart';
-export { default as Country } from './Country/Country';
-export { default as Cards } from './Cards/Cards';
-export { default as ChartCountry } from './ChartCountry/ChartCountry';
